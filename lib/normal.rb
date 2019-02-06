9e63016fc718065b8ad73470f39350b78270addf
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-06 13:22:01 +0000

    Refactor inline if statements

--- lib/normal.rb
+++ lib/normal.rb
@@ -3,9 +3,7 @@ require_relative 'item'
 class Normal < Item
 
   def update
-    unless @quality.zero?
-      @sell_in > 0 ? @quality -= 1 : @quality -= 2
-    end
+    @sell_in > 0 ? @quality -= 1 : @quality -= 2 unless @quality.zero?
     @sell_in -= 1 unless @sell_in.zero?
   end
 end
