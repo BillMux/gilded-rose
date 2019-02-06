e637edddf84554b1459de5b1c190daa11f1e2d27
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-06 13:19:02 +0000

    Change ln7 conditional, test passes

--- lib/normal.rb
+++ lib/normal.rb
@@ -4,7 +4,7 @@ class Normal < Item
 
   def update
     unless @quality.zero?
-      @sell_in >= 0 ? @quality -= 1 : @quality -= 2
+      @sell_in > 0 ? @quality -= 1 : @quality -= 2
     end
     @sell_in -= 1 unless @sell_in.zero?
   end
