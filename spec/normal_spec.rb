f4b16ffe757fc668530e97e2598c93f4de9900da
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-06 13:27:49 +0000

    Linting

--- spec/normal_spec.rb
+++ spec/normal_spec.rb
@@ -4,7 +4,7 @@ require 'item'
 describe 'Generic item' do
   context 'before sell_in' do
     it 'lowers quality by one after one day' do
-      item = Normal.new("item", 1, 1)
+      item = Normal.new('item', 1, 1)
       GildedRose.new([item]).update_quality
       expect(item.quality).to eq(0)
     end
@@ -12,7 +12,7 @@ describe 'Generic item' do
 
   context 'after sell_in' do
     it 'lowers quality by two after a day' do
-      item = Normal.new("item", 0, 2)
+      item = Normal.new('item', 0, 2)
       GildedRose.new([item]).update_quality
       expect(item.quality).to eq(0)
     end
--- spec/sulfuras_spec.rb
+++ spec/sulfuras_spec.rb
@@ -3,13 +3,13 @@ require 'item'
 
 describe 'Sulfuras, Hand of Ragnaros' do
   it 'does not change sell_in' do
-    item = Legendary.new("Sulfuras, Hand of Ragnaros", 1, 80)
+    item = Legendary.new('Sulfuras, Hand of Ragnaros', 1, 80)
     GildedRose.new([item]).update_quality
     expect(item.sell_in).to eq(1)
   end
 
   it 'does not change quality' do
-    item = Legendary.new("Sulfuras, Hand of Ragnaros", 1, 80)
+    item = Legendary.new('Sulfuras, Hand of Ragnaros', 1, 80)
     GildedRose.new([item]).update_quality
     expect(item.quality).to eq(80)
   end
