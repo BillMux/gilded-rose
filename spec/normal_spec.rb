48b56029e646b1380f9344bbc54ce753a28d48d7
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-06 13:04:21 +0000

    Move generic item tests to new file

---
+++ spec/normal_spec.rb
@@ -0,0 +1,20 @@
+require 'gilded_rose'
+require 'item'
+
+describe GildedRose do
+  context 'before sell_in' do
+    it 'lowers quality by one after one day' do
+      item = Normal.new("item", 1, 1)
+      GildedRose.new([item]).update_quality
+      expect(item.quality).to eq(0)
+    end
+  end
+
+  context 'after sell_in' do
+    it 'lowers quality by two after a day' do
+      item = Normal.new("item", 0, 2)
+      GildedRose.new([item]).update_quality
+      expect(item.quality).to eq(0)
+    end
+  end
+end
