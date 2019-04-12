f5798912b223510f59e419a918693b31bf28cdc3
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-12 14:47:37 +0100

    Remove commented doubles

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,11 +2,8 @@ require_relative 'spec_helper'
 require_relative '../lib/item'
 
 describe Item do
-  # let(:name) { double :name }
-  # let(:sell_in) { double :sell_in }
-  # let(:quality) { double :quality }
-
   subject { described_class.new('test item', 10, 10) }
+  
   it { is_expected.to respond_to :name, :sell_in, :quality }
 
   it 'convert to string form' do
